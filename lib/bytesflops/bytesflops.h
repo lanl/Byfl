f5cf89a937068282f3542a62745341de366b846d
Eric Anger <eanger@gmail.com>
2014-04-09 16:46:53 -0400

    InstIterator header has been relocated in LLVM.

--- lib/bytesflops/bytesflops.h
+++ lib/bytesflops/bytesflops.h
@@ -17,7 +17,7 @@
 #include "llvm/IR/Module.h"
 #include "llvm/PassManager.h"
 #include "llvm/Support/CommandLine.h"
-#include "llvm/Support/InstIterator.h"
+#include "llvm/IR/InstIterator.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
 #include <fstream>
