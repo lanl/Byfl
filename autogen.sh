c2a80385e3395c3c4a436ece34c80c9fed13bcf7
Scott Pakin <pakin@lanl.gov>
2014-09-16 17:43:59 -0600

    Automake foreign mode moved to configure.ac from autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -3,5 +3,5 @@
 # Generate all of the GNU Autotools helper files needed by Byfl.
 libtoolize --copy || exit 1
 aclocal || exit 1
-automake --add-missing --copy --foreign || exit 1
+automake --add-missing --copy || exit 1
 autoconf || exit 1
