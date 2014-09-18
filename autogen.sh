c1dc2e3c1a6aa3d3f015d89909d57aa53de72739
Scott Pakin <pakin@lanl.gov>
2014-09-18 14:25:35 -0600

    Removed autogen.sh
    The autogen.sh script wasn't doing anything that the standard
    autoreconf tool doesn't already do.
    
    This version of Byfl was tested against revision 218062 of LLVM,
    Clang, and DragonEgg.

--- autogen.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh -x
-
-# Generate all of the GNU Autotools helper files needed by Byfl.
-libtoolize --copy || exit 1
-aclocal || exit 1
-automake --add-missing --copy || exit 1
-autoconf || exit 1
