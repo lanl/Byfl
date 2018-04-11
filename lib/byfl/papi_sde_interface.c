dcdfce369aa9758311dd7a48bb138b23ad8b9423
Heike Jagode <jagode@icl.utk.edu>
2018-04-11 14:34:09 -0400

    Update BYFL-PAPI integration with API changes for the PAPI Software-defined Event (SDE) interface.

--- lib/byfl/papi_sde_interface.c
+++ lib/byfl/papi_sde_interface.c
@@ -10,27 +10,71 @@
 
 #pragma weak papi_sde_init
 #pragma weak papi_sde_register_counter
+#pragma weak papi_sde_register_fp_counter
+#pragma weak papi_sde_unregister_counter
 #pragma weak papi_sde_describe_counter
 
 papi_handle_t
 __attribute__((weak))
-papi_sde_init(const char *name_of_library, int event_count)
+papi_sde_init(const char *name_of_library)
 {
-  printf("weak papi_sde_init called from %s\n", __FILE__);
-  void * ptr = NULL;
-  return ptr;
+  (void) name_of_library;
+
+  return NULL;
 }
 
-void
+int
 __attribute__((weak))
 papi_sde_register_counter(papi_handle_t handle, const char *event_name, int cntr_mode, int cntr_type, void *counter)
 {
-  printf("weak papi_sde_register_counter called from %s\n", __FILE__);
+  (void) handle;
+  (void) event_name;
+  (void) cntr_mode;
+  (void) cntr_type;
+  (void) counter;
+
+  /* do nothing */
+
+  return 0;
 }
 
-void
+int
+__attribute__((weak))
+papi_sde_register_fp_counter(papi_handle_t handle, const char *event_name, int cntr_mode, int cntr_type, papi_sde_fptr_t func_ptr, void *param )
+{
+  (void) handle;
+  (void) event_name;
+  (void) cntr_mode;
+  (void) cntr_type;
+  (void) func_ptr;
+  (void) param;
+
+  /* do nothing */
+
+  return 0;
+}
+
+int
+__attribute__((weak))
+papi_sde_unregister_counter( void *handle, const char *event_name)
+{
+  (void) handle;
+  (void) event_name;
+
+  /* do nothing */
+
+  return 0;
+}
+
+int
 __attribute__((weak))
 papi_sde_describe_counter(papi_handle_t handle, const char *event_name, const char *event_description)
 {
-  printf("weak papi_sde_describe_counter called from %s\n", __FILE__);
+  (void) handle;
+  (void) event_name;
+  (void) event_description;
+
+  /* do nothing */
+
+  return 0;
 }
--- lib/byfl/papi_sde_interface.h
+++ lib/byfl/papi_sde_interface.h
@@ -8,10 +8,10 @@
 #define PAPI_SDE_INTERFACE_H
 
 /* 'cntr_mode' input parameter for papi_sde_register_counter */
-#define SDE_RO        0x00  // 0000 0000
-#define SDE_RW        0x01  // 0000 0001
-#define SDE_DELTA     0x00  // 0000 0000
-#define SDE_INSTANT   0x10  // 0001 0000
+#define PAPI_SDE_RO       0x00  // 0000 0000
+#define PAPI_SDE_RW       0x01  // 0000 0001
+#define PAPI_SDE_DELTA    0x00  // 0000 0000
+#define PAPI_SDE_INSTANT  0x10  // 0001 0000
 
 /* 'cntr_type' input parameter for papi_sde_register_counter */
 #define PAPI_SDE_long_long 0x0
@@ -20,16 +20,31 @@
 #define PAPI_SDE_float     0x3
 
 /* Interface to PAPI SDE functions */
+typedef long long int (*papi_sde_fptr_t)( void * );
 typedef void* papi_handle_t;
 
-papi_handle_t papi_sde_init(const char *name_of_library, int event_count);
-void papi_sde_register_counter(papi_handle_t handle, const char *event_name, int cntr_mode, int cntr_type, void *counter);
-void papi_sde_describe_counter(papi_handle_t handle, const char *event_name, const char *event_description);
+typedef struct papi_sde_fptr_struct_s {
+  papi_handle_t (*init)(const char *lib_name);
+  int (*register_counter)( void *handle, const char *event_name, int mode, int type, void *counter );
+  int (*register_fp_counter)( void *handle, const char *event_name, int mode, int type, papi_sde_fptr_t fp_counter, void *param );
+  int (*unregister_counter)( void *handle, const char *event_name);
+  int (*describe_counter)( void *handle, const char *event_name, const char *event_description );
+}papi_sde_fptr_struct_t;
 
-/* Required for papi_native_avail */
-void papi_sde_hook_list_events(
-    papi_handle_t (*sym_init)(const char *, int),
-    void          (*sym_reg)(void *, const char *, int, int, void *),
-    void          (*sym_desc)(void *, const char *, const char *));
+papi_handle_t papi_sde_init(const char *name_of_library);
+int papi_sde_register_counter(papi_handle_t handle, const char *event_name, int cntr_mode, int cntr_type, void *counter);
+int papi_sde_register_fp_counter(papi_handle_t handle, const char *event_name, int cntr_mode, int cntr_type, papi_sde_fptr_t func_ptr, void *param);
+int papi_sde_unregister_counter( void *handle, const char *event_name);
+int papi_sde_describe_counter(papi_handle_t handle, const char *event_name, const char *event_description );
+
+papi_handle_t papi_sde_hook_list_events( papi_sde_fptr_struct_t *fptr_struct);
+
+#define POPULATE_SDE_FPTR_STRUCT( _A_ ) do{\
+    _A_.init = papi_sde_init;\
+    _A_.register_counter = papi_sde_register_counter;\
+    _A_.register_fp_counter = papi_sde_register_fp_counter;\
+    _A_.unregister_counter = papi_sde_unregister_counter;\
+    _A_.describe_counter = papi_sde_describe_counter;\
+}while(0)
 
 #endif
